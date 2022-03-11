06da1c72bc90a88ed49269198eca3f41d0ed7d6f
maciej.spychalski <maciej.spychalski@gmail.com>
2022-03-11 14:31:32 +0100

    Added dom actions, connected dom elements and setup listeners

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import { mapListToDOMElements } from './dominteractions.js'
+
+class TvMaze {
+  constructor() {
+    this.viewElems = {}
+    this.showNameButtons = {}
+    this.selectedName = "harry"
+    this.initializeApp()
+  }
+
+  initializeApp = () => {
+    this.connectDOMElements()
+    this.setupListeners()
+  }
+
+  connectDOMElements = () => {
+    const listOfIds = Array.from(document.querySelectorAll('[id]')).map(elem => elem.id);
+    const listOfShowNames = Array.from(document.querySelectorAll('[data-show-name]'))
+      .map(elem => elem.dataset.showName)
+
+    this.viewElems = mapListToDOMElements(listOfIds, 'id')
+    this.showNameButtons = mapListToDOMElements(listOfShowNames, 'data-show-name')
+  }
+
+  setupListeners = () => {
+    Object.keys(this.showNameButtons).forEach(shwoName => {
+      this.showNameButtons[shwoName].addEventListener('click', this.SetCurrentNameFilter)
+    })
+  }
+
+  SetCurrentNameFilter = () => {
+    this.selectedName = event.target.dataset.showName
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', new TvMaze())
