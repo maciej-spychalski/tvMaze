6b0b122ffb1a88021033cad6a1431cffb8a2e692
maciej.spychalski <maciej.spychalski@gmail.com>
2022-03-12 20:28:14 +0100

    Added possibility to change displayed shows and added validation

--- src/app.js
+++ src/app.js
@@ -40,6 +40,8 @@ class TvMaze {
   }
 
   renderCards = shows => {
+      this.viewElems.showsWrapper.innerHTML = ""
+
       for (const show of shows) {
       this.createdShowCards(show.show)
     }
@@ -47,11 +49,22 @@ class TvMaze {
 
   createdShowCards = show => {
     const divCard = createDOMElem('div', 'card')
-    const img = createDOMElem('img', 'card-img-top', null, show.image.medium)
     const divCardBody = createDOMElem('div', 'card-body')
     const h5 = createDOMElem('h5', 'card-title', show.name)
-    const p = createDOMElem('p', 'card-text', show.summary)
     const btn = createDOMElem('button', 'btn btn-primary', 'Show details')
+    let img, p
+
+    if (show.image) {
+      img = createDOMElem('img', 'card-img-top', null, show.image.medium)
+    } else {
+      img = createDOMElem('img', 'card-img-top', null, 'https://via.placeholder.com/210x295')
+    }
+
+    if (show.summary) {
+      p = createDOMElem('p', 'card-text', `${show.summary.slice(0,80)}...`)
+    } else {
+      p = createDOMElem('p', 'card-text', 'There is no summary for that show yet.')
+    }
 
     divCard.appendChild(divCardBody)
     divCardBody.appendChild(img)
@@ -65,4 +78,3 @@ class TvMaze {
 }
 
 document.addEventListener('DOMContentLoaded', new TvMaze())
-
