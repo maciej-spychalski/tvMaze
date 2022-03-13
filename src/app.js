a51a7a63ef52fe4cc545f43806361b07405ebb81
maciej.spychalski <maciej.spychalski@gmail.com>
2022-03-13 18:26:34 +0100

    Added possibility to open and close modal correctly

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { mapListToDOMElements, createDOMElem } from './dominteractions.js'
-import { getShowsByKey } from './requests.js'
+import { getShowsByKey, getShowById } from './requests.js'
 
 class TvMaze {
   constructor() {
@@ -36,18 +36,40 @@ class TvMaze {
   }
 
   fetchAndDisplayShows = () => {
-    getShowsByKey(this.selectedName).then(shows =>this.renderCards(shows))
+    getShowsByKey(this.selectedName).then(shows => this.renderCardsOnList(shows))
   }
 
-  renderCards = shows => {
-      this.viewElems.showsWrapper.innerHTML = ""
+  renderCardsOnList = shows => {
+    Array.from(
+      document.querySelectorAll('[data-show-id]')
+    ).forEach(btn => btn.removeEventListener('click', this.openDetailsView))
 
-      for (const show of shows) {
-      this.createdShowCards(show.show)
+    this.viewElems.showsWrapper.innerHTML = ""
+
+    for (const { show } of shows) {
+      const card = this.createdShowCard(show, false)
+      this.viewElems.showsWrapper.appendChild(card)
     }
   }
 
-  createdShowCards = show => {
+  openDetailsView = event => {
+    const { showId } = event.target.dataset
+    getShowById(showId).then(show => {
+      const card = this.createdShowCard(show, true)
+      this.viewElems.showPreview.appendChild(card)
+      this.viewElems.showPreview.style.display = 'block'
+    })
+  }
+
+  closeDetailsView = event => {
+    const { showId } = event.target.dataset
+    const closeBtn = document.querySelector(`[id="showPreview"] [data-show-id="${showId}"]`)
+    closeBtn.removeEventListener('click', this.closeDetailsView)
+    this.viewElems.showPreview.style.display = 'none'
+    this.viewElems.showPreview.innerHTML = ''
+  }
+
+  createdShowCard = (show, isDetailed) => {
     const divCard = createDOMElem('div', 'card')
     const divCardBody = createDOMElem('div', 'card-body')
     const h5 = createDOMElem('h5', 'card-title', show.name)
@@ -55,15 +77,32 @@ class TvMaze {
     let img, p
 
     if (show.image) {
-      img = createDOMElem('img', 'card-img-top', null, show.image.medium)
+      if (isDetailed) {
+        img = createDOMElem('div', 'card-preview-bg')
+        img.style.backgroundImage = `url('${show.image.original}')`
+      } else {
+        img = createDOMElem('img', 'card-img-top', null, show.image.medium)
+      }
     } else {
       img = createDOMElem('img', 'card-img-top', null, 'https://via.placeholder.com/210x295')
     }
 
     if (show.summary) {
-      p = createDOMElem('p', 'card-text', `${show.summary.slice(0,80)}...`)
+      if (isDetailed) {
+        p = createDOMElem('p', 'card-text', show.summary)
+      } else {
+        p = createDOMElem('p', 'card-text', `${show.summary.slice(0, 80)}...`)
+      }
     } else {
       p = createDOMElem('p', 'card-text', 'There is no summary for that show yet.')
+    }
+
+    btn.dataset.showId = show.id
+
+    if (isDetailed) {
+      btn.addEventListener('click', this.closeDetailsView)
+    } else {
+      btn.addEventListener('click', this.openDetailsView)
     }
 
     divCard.appendChild(divCardBody)
@@ -72,7 +111,7 @@ class TvMaze {
     divCardBody.appendChild(p)
     divCardBody.appendChild(btn)
 
-    this.viewElems.showsWrapper.appendChild(divCard)
+    return divCard
   }
 
 }
